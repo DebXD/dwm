f54fb5f27c06de9f865fa5823e1adc6ebf1ce2df
Debiprasad Das <debiprasadxd@protonmail.com>
2024-06-08 05:44:40 +0530

    remove firefox & gimp window rules

--- config.def.h
+++ config.def.h
@@ -45,8 +45,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "st-256color",NULL,  NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",NULL,  NULL,           0,         0,          1,           0,        -1 },
